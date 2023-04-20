c18d5265b0219c54793c948059f10b1988b4d340
Abcdefgdude <benyoung32@gmail.com>
2023-04-19 20:55:04 -0700

    update

--- Problem2.R
+++ Problem2.R
@@ -5,7 +5,7 @@ ExactAnalysis <- function() {
   k <- 1
   r <- 3
   q <- 1
-  nDays <- 100
+  nDays <- 10000
   " k = 1, m = 5, P(bus gets there at t=6) = P(bus leaves the station at 6 -v = 4) < 1 wait
   - P(bus leaves at station 5 - v = 3) < 0 wait
   P(bus leaves at 4) = 
@@ -22,7 +22,7 @@ ExactAnalysis <- function() {
   P(L_1 = 2 and L_2 = 2) = 0.5 * 0.5
   + P(L_1 = 1 and L_2 = 1 and L_# = 2) = 0.5 * 0.5 * 0.5 
   = 0.5^2 + 0.5^3 = 0.3675 "
-  p_wEqualk <- 0.5^2 + 3*(0.5^3) + 0.5^4
+  p_wEqualk <- 0.5^2 + 0.5^3
   "P(B_2 leaves at time r = 3) 
    There are two scenarios where B_2 leaves at r = 3, L_1 = 1 and L_2 = 2,
   or L_1 = 2 and L_2 = 1. "
@@ -33,12 +33,12 @@ ExactAnalysis <- function() {
   buses must leave with delays L_2 = 1 and L_3 = 2 for W = 1. 
   So, P(W = 1 and L_1 = 1) / P(L_1) = 0.5^3 / 0.5"
   p_WequalKgivenL1eqQ <- (p[1] * p[1] * p[2]) / p[1]
-  "There is only one way for the first bus to arrive to be the third bus, that is 
-  the first three buses leave with delay 1."
+  "There are two ways for the first bus to arrive to be the third bus, that is 
+  the first three buses leave with delays (1,1,1) or (1,1,2). So the probability
+  that the passenger boards the third bus is 2 * 0.5^3"
   p_Ueq3 <- p[1]^3
 
   print(busSim(m,p,v,k,r,q,nDays))
-  # return (list(p_wEqualk,p_bus2LeavesAtR, p_WequalKgivenL1eqQ))
   return (c(p_wEqualk, p_WequalKgivenL1eqQ))
 }
 # in our "notebook," 1 line = 1 day
@@ -60,21 +60,16 @@ busSim <- function(m,p,v,k,r,q,nDays)
   # is proportion of 1s
   p_wEqualk <- mean(wVals == k)
   p_bus2LeavesAtR <- mean(bus2LeavesByR)
-    for (day in 1:nDays) wVals[day] <- generateW(v,p,m-1)[1]
-  p_WequalKgivenL1eqQ <- mean(wVals == k)
   p_Ueq3 <- mean(busCounts == 3)
   exp_Wait <- mean(wVals)
   var_W <- mean(wVals^2) - (mean(wVals))^2
+    for (day in 1:nDays) wVals[day] <- generateW(v,p,m-1)[1]
+  p_WequalKgivenL1eqQ <- mean(wVals == k)
   # E(buses) = long running average
   exp_Bus <- mean(busCounts)
   exp_numBusesByM <- mean(busCountsByM)
   # Var(buses) = E(buses^2) - (E(buses))^2
   var_numBusesByM <- mean(busCountsByM^2) - (mean(busCountsByM))^2
-  print(mean(busCountsByM == 2))
-  print(mean(busCountsByM == 3))
-  print(mean(busCountsByM == 4))
-  print(mean(busCountsByM == 5))
-  print(busCountsByM)
   for (day in 1:nDays) busCounts[day] <- generateW(v,p,m,k)[2]
   exp_numBusesLeavingGivenWeqK <- mean(busCounts)
   return (c(p_wEqualk,p_bus2LeavesAtR, p_WequalKgivenL1eqQ,p_Ueq3,exp_Wait, var_W, 
@@ -99,12 +94,11 @@ generateBusByM <- function(p,m)
   buses <- 0 # count of buses
   total <- 0 # how much total delay has there been
   while (1) {
-    buses <- buses + 1
+    buses <- buses + 1 # another bus left
     total <- total + generateL(p)
     if (total>m) return(buses-1) # if total goes above m, take away the last bus
     if (total==m) return(buses) # stop counting at time m 
   }
-  return (buses) # return count 
 }
 
 generateW <- function(v,p,m,k=-1) 
@@ -112,10 +106,11 @@ generateW <- function(v,p,m,k=-1)
   buses <- 0 # count of buses
   tot <- v # how much total delay has there been
   while (1) {
-    buses <- buses + 1
+    buses <- buses + 1 # another bus left
     tot <- tot + generateL(p)
-    if (tot >= m) break
+    if (tot >= m) break # stop counting once bus has arrived at the stop
   }
+  # if a given k value was passed, only accept when the wait = k 
   if (k != -1 & tot - m != k) return (generateW(v,p,m,k)) 
   return(c(tot-m,buses))
 }
