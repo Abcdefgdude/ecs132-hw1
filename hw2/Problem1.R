11ede307515640900af61db30b9d7b97ffb6cb97
Abcdefgdude <benyoung32@gmail.com>
2023-04-30 10:56:10 -0700

    problem 1 first draft

---
+++ hw2/Problem1.R
@@ -0,0 +1,33 @@
+PAMsim <- function(nGen) {
+  rows <- nGen + 2
+  cols <- nGen + 2
+  attachHistory <- vector(length = nGen)
+  adjMat <- matrix(data=0, nrow = rows, ncol = cols)
+  # nodes 1 and 2 begin connected
+  adjMat[1,2:5] <- 1
+  adjMat[2,1] <- 1 
+  for (i in 1:nGen) {
+    # p is the vector of weights for each node
+    p <- vector(length = rows)
+    # calc the weight of each node as the proportion of connections
+    for (j in 1:rows) p[j] <- sum(adjMat[j,]/sum(adjMat))
+    # select new node based on above weights
+    newNode <- sample(1:rows,1,replace = FALSE, prob = p)
+    # connect nodes in adjacency matrix
+    adjMat[i+2,newNode] <- 1
+    adjMat[newNode, i+2] <- 1 
+    # record node in history
+    attachHistory[i] <- newNode
+  }
+  return (list(adjMat, attachHistory))
+}
+
+PAMemaxd <- function(nGen, nReps) {
+  maxdegrees <- vector(length=nReps)
+  for (i in 1:nReps) {
+    attachHistory <- PAMsim(nGen)[2]
+    attachTable <- sort(table(attachHistory),decreasing=TRUE)
+    maxdegrees[i] <- attachTable[1]
+  }
+  return (mean(maxdegrees))
+}
