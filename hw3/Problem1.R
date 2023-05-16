79a6901b7ca99fef9dea6231bfccc75dfc2b1d72
Abcdefgdude <benyoung32@gmail.com>
2023-05-16 10:51:26 -0700

    creating files/function headers

---
+++ hw3/Problem1.R
@@ -0,0 +1,12 @@
+dpark <- function(p,d,i) {
+  
+}
+ppark <- function(p,d,i) {
+  
+}
+qpark <- function(p,d,q) {
+  
+}
+rpark <- function(n,p,d) {
+  
+}
---
+++ hw3/Problem2.R
@@ -0,0 +1,3 @@
+boardGame <- function() {
+  
+}
