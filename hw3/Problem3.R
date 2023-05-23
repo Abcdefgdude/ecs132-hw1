1177ffc88695d02bb0e42f54083e39ac7b02555c
Abcdefgdude <benyoung32@gmail.com>
2023-05-23 10:51:55 -0700

    cooking

--- hw3/Problem3.R
+++ hw3/Problem3.R
@@ -1,3 +1,43 @@
-callCtr <- function(p,q,r,w) {
+callCtr <- function(p, q, r, w) {
+  # Transition probability matrix
+  num_states <- r + w
+  P <- matrix(0, nrow = num_states, ncol = num_states)
   
+  # Set up transition probabilities for each state
+  for (i in 1:num_states) { # i represents the state where we have i calls active + on hold
+    if (i == 1) { 
+      # No calls in the system
+      P[i,i] = 1 - q # a new call does not arrive
+      P[i,2] = q # a new call does arrive
+    } else if (i <= r) {
+      for (j in 0:i)  {# how many existing calls finish
+        print(dbinom(j,i,p))
+        P[i,i-j+1] <- P[i,i-j+1] + dbinom(j,i,p)*(q) # j calls finish, a new call comes in
+        P[i,i-j] <- P[i,i-j] + dbinom(j,i,p)*(1-q) # j calls finish, no new call comes in
+      }
+    } else if (i < r + w) {
+      # Calls in the waiting area
+      # a maximum of r calls could finish
+      for (j in 0:r) {
+        P[i,i-j+1] <- P[i,i-j+1] + dbinom(j,r,p)*(q) # j calls finish, a new call comes in
+        P[i,i-j] <- P[i,i-j] + dbinom(j,r,p)*(1-q) # j calls finish, no new call comes in
+      }
+    } else {
+      # Maximum number of calls reached
+      # a maximum of r calls could finish
+      # if no calls finish, we will always stay in the max call state (whether new call comes in or not)
+      P[i,i] <- dbinom(0,r,p)
+      for (j in 1:r) {
+        P[i,i-j+1] <- P[i,i-j+1] + dbinom(j,r,p)*(q) # j calls finish, a new call comes in
+        P[i,i-j] <- P[i,i-j] + dbinom(j,r,p)*(1-q) # j calls finish, no new call comes in
+      }
+    }
+  }
+  
+  # Calculate long-run proportions and averages
+  
+  #return(results)
+  return(P)
 }
+results <- callCtr(0.5, 0.5, 5, 3)
+print(results)
