1fc37a046dc97e2ef32e67796c8f2a60e6733cc4
Abcdefgdude <benyoung32@gmail.com>
2023-05-23 11:16:49 -0700

    finish?

--- hw3/Problem3.R
+++ hw3/Problem3.R
@@ -1,3 +1,12 @@
+# pi1 function from book
+findpi1 <- function(p) {
+  n <- nrow(p)
+  imp <- diag(n) - t(p)
+  imp[n,] <- rep(1,n)
+  rhs <- c(rep(0,n-1),1)
+  solve(imp,rhs)
+}
+
 callCtr <- function(p, q, r, w) {
   # Transition probability matrix
   num_states <- r + w
@@ -11,7 +20,6 @@ callCtr <- function(p, q, r, w) {
       P[i,2] = q # a new call does arrive
     } else if (i <= r) {
       for (j in 0:i)  {# how many existing calls finish
-        print(dbinom(j,i,p))
         P[i,i-j+1] <- P[i,i-j+1] + dbinom(j,i,p)*(q) # j calls finish, a new call comes in
         P[i,i-j] <- P[i,i-j] + dbinom(j,i,p)*(1-q) # j calls finish, no new call comes in
       }
@@ -33,11 +41,17 @@ callCtr <- function(p, q, r, w) {
       }
     }
   }
-  
   # Calculate long-run proportions and averages
-  
+  pi <- findpi1(P)
+  # probability that a call is dropped = 
+  # P(full calls) * P(no calls dropped) * P(new call)
+  call_dropped <- pi[r+w] * dbinom(0,r,p) * q
+  # average number of calls on hold = sum(1,w) of P(holds = i) * i
+  average_holds <- (1:w) %*% pi[(r+1):(r+w)]
   #return(results)
-  return(P)
+  # average number of busy operators = sum(1,r) of P(calls = i) * i + sum(r,r+w) P(holds = i) * i
+  average_calls <- (1:r) %*% pi[1:r] + rep(r,w) %*% pi[(r+1):(r+w)]
+  return(c(call_dropped,average_holds,average_calls))
 }
 results <- callCtr(0.5, 0.5, 5, 3)
 print(results)
