badb849a5d3217fa48aaf5c566cfb030242cd650
mutthias <matthiasj.gabel@gmail.com>
2023-05-23 21:22:47 -0700

    hw 2 matt

--- hw3/Problem2.R
+++ hw3/Problem2.R
@@ -1,59 +1,55 @@
-# pi1 function from book
-findpi1 <- function(p) {
+boardGame <- function() {
+  p <- matrix(rep(0, 64), nrow = 8) # - 8 total states, so 8x8 matrix with 0's
+  for (i in 1:8) {                  # - "Starting from square i..."
+    for (j in 1:6) {                # - "...what is the probability we land on j?" 6 possible rolls
+      k <- i + j                    
+      if (k > 8) k <- k - 8         # - If k < 8, wrap back to the beginning
+      if (k == 4) p[i,k] <- 0       # - If k == 4, we hit a bonus, set to 0 since we can't end here
+      else p[i,k] <- 1/6            # - There is a 1/6 probability of rolling k from square i
+    }
+    if (i != 4 & i != 5) {          # - If i == 4 | 5, we can't hit the bonus 
+      for (m in 1:6) {              # - Otherwise, we add 1/36 to each square that can be rolled
+                                    #   within square 4 (1/6 chance to roll bonus, then 1/6 chance
+                                    #   to roll i from square 4: 1/6 * 1/6 = 1/36)
+        m <- 4 + m
+        if (m > 8) m <- m - 8       # If past 8 wrap back to square 1
+        p[i,m] <- p[i,m] + 1/36     
+      }
+    }
+  }
+  pi <- findpi(p)
+  expectedW <- mean(calculateW(p))
+  varW <- mean(calculateW(p)^2) - (mean(calculateW(p)))^2
+  return(c(pi, expectedW, varW))
+}
+
+calculateW <- function(p) {
+  # Expected value =
+  # sum for i in 1:8 -> P(square = i) * (P(win|square=i) + P(2win|square=i))
+  # P(win|square=i) = probability goes to state > 8
+  # P(2win|square=i) = probability goes to state = 4 and goes to state > 8
+  
+  n <- nrow(p)
+  winnings <- vector(length = n)
+  EW <- 0
+  for (i in 1:n) {
+    if (i + 6 == n) 
+      highest <- n
+    else 
+      highest <- (i + 6) %% n                   # check if you can possibly win (not possible from 1 or 2)
+    if (highest < i)                            # eg if i = 6, highest = 6 + 6 %% 8 = 4. 4 out of 6 possible rolls are winning
+      winnings[i] <- highest / 6
+    if (i != 4 & i != 5)                        # 4 is reachable from every square but 4 and 5
+      winnings[i] <- winnings[i] + (2/6) * 1/6  # you have 2/6 chance to win from 4, and 1/6 chance to land on 4
+  }
+  return(winnings)
+}
+
+# Findpi function from the book
+findpi <- function(p) {
   n <- nrow(p)
   imp <- diag(n) - t(p)
   imp[n,] <- rep(1,n)
-  rhs <- c(rep(0,n-1),1)
-  solve(imp,rhs)
+  rhs <- c(rep(0, n-1), 1)
+  solve(imp, rhs)
 }
-
-boardGame <- function() {
-  l <- 8 # number of squares
-  b <- 4 # bonus square
-  p <- matrix(data=0,nrow=8,ncol=8)
-  # generate p by simulating possible rolls
-  for (i in 1:l) { # start from any state
-    for (j in 1:6) { # roll die from 1:6
-      if ((i + j) == l) square <- l # modulo edge case
-      else square <- (i + j) %% l
-      if (square == b)  # you are on bonus square, add probs to next squares
-          for (k in 1:6) {
-            if ((square + k) == l) square2 <- l
-            else square2 <- (square + k) %% l
-          # add 1/6^2 since you have 1/6 chance to land on bonus, times 1/6 to get the k from bonus 
-            p[i,square2] <- p[i,square2] + (1/6)^2 
-          }
-      else {
-        # 1/6 chance to land on this square
-        p[i,square] <- p[i,square] + 1/6
-      }
-    }
-    print(p)
-    pi <- findpi1(p)
-    # Expected value =
-    # sum for i in 1:8 -> P(square = i) * (P(win|square=i) + P(2win|square=i))
-    # P(win|square=i) = probability goes to state > 8
-    # P(2win|square=i) = probability goes to state = 4 and goes to state > 8
-    EW <- 0 # expected winnings running sum
-    winnings <- vector(length=l)
-    for (i in 1:l) { # loop over each square
-      if ((i + 6) == l) highest <- l
-      else highest <- (i+6) %% l
-      # check if you can possibly win (not possible from 1 or 2)
-      print(highest)
-      if (highest < i) {
-        # eg if i = 6, highest = 6 + 6 %% 8 = 4. 4 out of 6 possbile rolls are winning
-        winnings[i] <- highest/6 
-      }
-      # 4 is reachable from every square but 4 and 5
-      if (i != 4 & i != 5) {
-        winnings[i] <- winnings[i] + (2/6) * 1/6 # you have 2/6 chance to win from 4, and 1/6 chance to land on 4
-      }
-    }
-    EW <-  mean(winnings)
-    print(winnings %*% pi)
-    
-  }
-
-}
-boardGame()
